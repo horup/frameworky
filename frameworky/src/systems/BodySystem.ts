e0c41e28f095c61ffbc9efd81360c7abbc6fcc0d
horup <sah@sah.dk>
2020-08-26 07:56:14 +0200

    added cannon.js

--- frameworky/src/systems/BodySystem.ts
+++ frameworky/src/systems/BodySystem.ts
@@ -2,15 +2,16 @@ import { System } from "../System";
 import { Frameworky } from "../Frameworky";
 import { BaseEntity } from "../BaseEntity";
 import { BaseCommand } from "../BaseCommand";
-import {Engine, Bodies, Body, World} from 'matter-js';
+//import {Engine, Bodies, Body, World} from 'matter-js';
+import * as CANNON from 'cannon';
+
 
 export class BodySystem implements System<BaseEntity, BaseCommand>
 {
-    engine:Engine = Engine.create();
-    bodies = new Map<number, Body>();
+    world = new CANNON.World();
+    bodies = new Map<number, CANNON.Body>();
     init(f: Frameworky<BaseEntity>) {
-        //console.log(this.engine.world.gravity.y);
-        this.engine.world.gravity.y = 0;
+        this.world.gravity.set(0,0, -9.82);
     }
 
     executeCommand(f: Frameworky<BaseEntity>, command: BaseCommand) 
@@ -22,23 +23,30 @@ export class BodySystem implements System<BaseEntity, BaseCommand>
                 const m = e.transform.get();
                 if (this.bodies.has(e.id) == false)
                 {
-                    const circle = Bodies.circle(m.x, m.y, 0.5);
+                    const circle = new CANNON.Body({
+                        mass: 5,
+                        position: new CANNON.Vec3(m.x, m.y, m.z),
+                        shape: new CANNON.Sphere(0.5)
+                    })
+                    this.world.addBody(circle);
                     this.bodies.set(e.id, circle);
-                    World.add(this.engine.world, [circle]);
                 }
                 else
                 {
                     this.bodies.get(e.id).position.x = m.x;
                     this.bodies.get(e.id).position.y = m.y;
+                    this.bodies.get(e.id).position.z = m.z;
                 }
             }, e=>e.body.has && e.transform.has);
-            Engine.update(this.engine);
+            
+            this.world.step(command.fixedUpdate.tickRate);
 
             // ensure bodies a syncronized back to transform
             this.bodies.forEach((b,id)=>{
                 const m = f.entityManager.get(id).transform.get();
                 m.x = b.position.x;
                 m.y = b.position.y;
+                m.z = b.position.z;
             })
         }
        /* if (command.deleteEntity)
